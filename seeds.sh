4445a379a5ccfc4d40086ad672f275ac27cace81
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2022-12-20 09:45:44 +0100

    [feat] added multiple questions in seeds.sh

--- seeds.sh
+++ seeds.sh
@@ -4,3 +4,21 @@ curl -d "{
   \"validAnswers\": [\"Stone\", \"Rock\", \"Basalt\"],
   \"invalidAnswers\": [\"Cheese\", \"Ashes\", \"Garlic\", \"Lava\", \"Rubber\"]
 }" http://localhost/api/add-question
+
+curl -d "{
+  \"question\": \"What's the Moon made of?\",
+  \"validAnswers\": [\"Stone\", \"Rock\", \"Basalt\"],
+  \"invalidAnswers\": [\"Cheese\", \"Ashes\", \"Garlic\", \"Lava\", \"Rubber\"]
+}" http://localhost/api/add-question
+
+curl -d "{
+  \"question\": \"Is Elon Musk CEO of Twitter?\",
+  \"validAnswers\": [\"Yes\"],
+  \"invalidAnswers\": [\"No\"]
+}" http://localhost/api/add-question
+
+curl -d "{
+  \"question\": \"Does chicken have teeth?\",
+  \"validAnswers\": [\"No\"],
+  \"invalidAnswers\": [\"Yes\"]
+}" http://localhost/api/add-question
